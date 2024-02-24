caa931e5309db200042a1c2c00c2ad7459e259a0
danieleangeletti <angelettidaniele@gmail.com>
2024-02-24 07:17:16 +0100

    ADD foreach per le img del main-two

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,3 +1,28 @@
+@php
+    $buy_img = [
+        [
+            'img_path' => 'resources/img/buy-comics-digital-comics.png',
+            'title' => 'DIGITAL COMICS'
+        ],
+        [
+            'img_path' => 'resources/img/buy-comics-merchandise.png',
+            'title' => 'DC MERCHANDISE'
+        ],
+        [
+            'img_path' => 'resources/img/buy-comics-subscriptions.png',
+            'title' => 'SUBSCRIPTION'
+        ],
+        [
+            'img_path' => 'resources/img/buy-comics-shop-locator.png',
+            'title' => 'COMIC SHOP LOCATOR'
+        ],
+        [
+            'img_path' => 'resources/img/buy-dc-power-visa.svg',
+            'title' => 'DC POWER VISA'
+        ]
+    ]
+@endphp
+
 @extends('layouts.app')
 
 @section('page-title', 'Home')
@@ -36,36 +61,14 @@
         <div class="d-flex justify-content-center main-two-bg main-two-height">
             <div class="h-100 my-container">
                 <div class="h-100 d-flex align-items-center">
-                    <div class="h-40 grow-one smaller-plus">
-                        <div class="h-100 d-flex justify-content-center align-items-center">
-                            <img class="h-100 ps-8 pe-8" src="{{Vite::asset('resources/img/buy-comics-digital-comics.png')}}" alt="" />
-                            <div class="white">DIGITAL COMICS</div>
+                    @foreach ($buy_img as $elem)
+                        <div class="h-40 grow-one smaller-plus">
+                            <div class="h-100 d-flex justify-content-center align-items-center">
+                                <img class="h-100 ps-8 pe-8" src="{{Vite::asset($elem['img_path'])}}" alt="" />
+                                <div class="white">{{ $elem['title'] }}</div>
+                            </div>
                         </div>
-                    </div>
-                    <div class="h-40 grow-one smaller-plus">
-                        <div class="h-100 d-flex justify-content-center align-items-center">
-                            <img class="h-100 ps-8 pe-8" src="{{Vite::asset('resources/img/buy-comics-merchandise.png')}}" alt="" />
-                            <div class="white">DC MERCHANDISE</div>
-                        </div>
-                    </div>
-                    <div class="h-40 grow-one smaller-plus">
-                        <div class="h-100 d-flex justify-content-center align-items-center">
-                            <img class="h-100 ps-8 pe-8" src="{{Vite::asset('resources/img/buy-comics-subscriptions.png')}}" alt="" />
-                            <div class="white">SUBSCRIPTION</div>
-                        </div>
-                    </div>
-                    <div class="h-40 grow-one smaller-plus">
-                        <div class="h-100 d-flex justify-content-center align-items-center">
-                            <img class="h-100 ps-8 pe-8" src="{{Vite::asset('resources/img/buy-comics-shop-locator.png')}}" alt="" />
-                            <div class="white">COMIC SHOP LOCATOR</div>
-                        </div>
-                    </div>
-                    <div class="h-40 grow-one smaller-plus">
-                        <div class="h-100 d-flex justify-content-center align-items-center">
-                            <img class="h-100 ps-8 pe-8" src="{{Vite::asset('resources/img/buy-dc-power-visa.svg')}}" alt="" />
-                            <div class="white">DC POWER VISA</div>
-                        </div>
-                    </div>
+                    @endforeach
                 </div>
             </div>
         </div>
