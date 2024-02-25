d91e76afb2ab98660c63d14977d10dc0ebfa7802
danieleangeletti <angelettidaniele@gmail.com>
2024-02-25 18:27:52 +0100

    inizio comics page

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -7,6 +7,9 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>@yield('page-title') | Laravel Start 1</title>
 
+    {{-- font-awesome --}}
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css" integrity="sha512-DTOQO9RWCH3ppGqcWaEA1BIZOC6xxalwEsw9c2QQeAIftl+Vegovlnee1c9QX4TctnWMn13TZye+giMm8e2LwA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+
     {{-- Stili nell'head --}}
     <link rel="stylesheet" href="css/style.css">
 
--- resources/views/subpages/comics.blade.php
+++ resources/views/subpages/comics.blade.php
@@ -3,7 +3,29 @@
 @section('page-title', 'Comics')
 
 @section('main-content')
-<h1>
-    This is the Comics page
-</h1>
+    <main>
+        <div class="my-container-two">
+            <div class="row">
+                <div class="col-8">
+                    <h5>
+                        ACTION COMICS #1000: THE DELUXE EDITION
+                    </h5>
+                    <div class="d-flex green text-white">
+                        <div class="width-70 d-flex align-items-center justify-content-between border-end p-2">
+                            <span>U.S. Price:$19.99</span>
+                            <span class="pe-1 opacity-50">AVAILABLE</span>
+                        </div>
+                        <div class="width-30 d-flex align-items-center p-2">
+                            <p class="ps-1 m-0">
+                                Check Availability <i class="fa-solid fa-chevron-down"></i>
+                            </p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-4">
+                    COL-4
+                </div> 
+            </div>
+        </div>
+    </main>
 @endsection
